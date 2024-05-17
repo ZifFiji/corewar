314cb1985e8accc6615c5d1a57379ece2753bf32
Moana PIQUET <moana.piquet@epitech.eu>
2024-05-17 17:50:00 +0200

    [FIX] fix lldi because have idx mod

--- src/instructions/lldi.c
+++ src/instructions/lldi.c
@@ -7,18 +7,19 @@
 
 #include "my.h"
 
+static
 void add_value(corewar_t *cw, champions_t *c, int *args)
 {
     size_t adress = 0;
     size_t sum = 0;
     int value = 0;
 
-    adress = ((c->program_counter + args[0]) % IDX_MOD) % MEM_SIZE;
+    adress = (c->program_counter + args[0]) % MEM_SIZE;
     for (size_t i = 0; i < IND_SIZE + adress; i++) {
         sum += cw->arena[adress];
         adress++;
     }
-    adress = ((c->program_counter + sum) % IDX_MOD) % MEM_SIZE;
+    adress = (c->program_counter + sum) % MEM_SIZE;
     for (int i = 0; i < REG_SIZE; i++) {
         value += cw->arena[adress];
         adress++;
